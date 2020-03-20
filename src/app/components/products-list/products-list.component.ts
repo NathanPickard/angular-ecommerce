08a951308deac5ce7826eb7b1ea20d3c7acf0046
Nathan Pickard <nathan.pickard87@gmail.com>
2020-03-19 20:57:29 -0700

    Product service subscription

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from 'src/app/services/products.service';
+import { Product } from 'src/app/models/Product';
 
 @Component({
   selector: 'app-products-list',
@@ -7,10 +8,19 @@ import { ProductsService } from 'src/app/services/products.service';
   styleUrls: ['./products-list.component.scss']
 })
 export class ProductsListComponent implements OnInit {
+  products: Product[] = [];
+  loading = false;
 
   constructor(private productsService: ProductsService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loading = true;
+
+    this.productsService.getAllProducts()
+      .subscribe(products => {
+        this.loading = false;
+        this.products = products;
+      });
   }
 
 }
