81747702b9c98a35635066d411dde282af8afd94
Nathan Pickard <nathan.pickard87@gmail.com>
2020-03-21 15:47:16 -0700

    Product list item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductsListItemComponent } from './componets/products-list-item/products-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsListComponent
+    ProductsListComponent,
+    ProductsListItemComponent
   ],
   imports: [
     BrowserModule,
